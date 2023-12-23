78aecd2820050f7386b36f5198fb56f8728fe7d6
Kanstantsin Davydovich <kostiarabota@gmail.com>
2023-12-23 18:07:12 +0300

    feat: add dateSample funktion

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) !== 'string'){  return false; }
+  sampleActivity = Number(sampleActivity);
+  if (!!sampleActivity & sampleActivity > 0 & sampleActivity <= MODERN_ACTIVITY) {
+    const years = Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD);
+    return Math.ceil(years);
+  }
+  return false;
+} 
   // remove line with error and write your code here
-}
+
 
 module.exports = {
   dateSample
