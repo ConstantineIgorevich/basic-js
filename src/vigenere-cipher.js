72344ba45dd2b1cb159a4315d85036b77d44510e
Kanstantsin Davydovich <kostiarabota@gmail.com>
2023-12-26 02:52:44 +0300

    feat: add viginer begin

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+
+  constructor (type) {
+    if (type === false){
+      this.type = type;
+    } else {
+      this.type = true;
+    }
+  }
+
+  encrypt(message, key) {
+    if (!message | !key) {
+      throw Error("Incorrect arguments!");
+    }
+    let messageForEncrypt = message.replaceAll(' ', '').toUpperCase();
+    let keyForEncrypt = key.replaceAll(' ', '').toUpperCase();
+    let encryptedMessage = [];
+    messageForEncrypt = messageForEncrypt.split('');
+    keyForEncrypt = keyForEncrypt.split('');
+    
+      for (let i = 0, j = 0; i < messageForEncrypt.length; i++) {
+        if (i > keyForEncrypt.length - 1) {
+          keyForEncrypt[i] = keyForEncrypt[j];
+          j++;
+          if (j === keyForEncrypt.length) {
+            j = 0;
+          }
+        }
+        let charM = messageForEncrypt[i].charCodeAt();
+        let charK = keyForEncrypt[i].charCodeAt();
+        if (charM > 64 && charM < 91 && charK > 64 && charK < 91) {
+          if (charM + (charM - charK) < 91) {
+            charM += (charM - charK);
+          } else {
+            charM = 64 + ((charM + (charM - charK)) % 90);
+          }
+        encryptedMessage[i] = String.fromCharCode(charM);
+      } else {
+        encryptedMessage[i] = messageForEncrypt[i];
+      }
+    }
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(message, key) {
+    if (!message | !key) {
+      throw Error("Incorrect arguments!");
+    }
     // remove line with error and write your code here
   }
 }
