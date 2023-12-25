fd2238154c3fb112e941dfade637ebb8e6eb6006
Kanstantsin Davydovich <kostiarabota@gmail.com>
2023-12-26 01:03:55 +0300

    feat: add  chainMaker object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  property: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.property.length;
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    if (value !== undefined){
+      this.property.push(`( ${value} )`);
+    } else {
+      this.property.push(`( )`);
+    }
+    return this;
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    if (position < 1 | position > this.getLength() | typeof position !== 'number') {
+      this.property = [];
+      throw Error("You can't remove incorrect link!");
+    } else {
+      this.property.splice(position - 1, 1);
+      return this;
+    }
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.property.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    const chain = this.property.join('~~');
+    this.property = [];
+    return chain;
     // remove line with error and write your code here
   }
 };
